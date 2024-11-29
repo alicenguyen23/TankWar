b904e64d6f63b0c2cbbdaf4c4b97e8cb5239b34a
nguyetnguyenmv <nguyetnguyenmv@mvillage.com>
2024-11-29 01:25:55 -0600

    feat: [BASIC STRUCTURE] Create a base GameObject class

---
+++ src/main/java/edu/tcu/cs/tankwar/model/GameObject.java
@@ -0,0 +1,58 @@
+package edu.tcu.cs.tankwar.model;
+
+import javafx.geometry.Point2D;
+import javafx.scene.canvas.GraphicsContext;
+
+public abstract class GameObject {
+  /* Basic properties */
+  protected Point2D position; /* x and y coordinates */
+  protected double width; /* object's dimension */
+  protected double height; /* object's dimension */
+  protected boolean isActive; /* if object is still active */
+
+  public GameObject(double x, double y, double width, double height) {
+    this.position = new Point2D(x, y);
+    this.width = width;
+    this.height = height;
+    this.isActive = true;
+  }
+
+  /* Update game logic */
+  public abstract void update(double deltaTime);
+  /* Render game object */
+  public abstract void render(GraphicsContext gc);
+
+  /* Collision detection method */
+  public boolean collides(GameObject other) {
+    return position.getX() < other.position.getX() + other.width &&
+            position.getX() + width > other.position.getX() &&
+            position.getY() < other.position.getY() + other.height &&
+            position.getY() + height > other.position.getY();
+  }
+
+  /* Getters and setters */
+
+  public Point2D getPosition() {
+    return position;
+  }
+
+  public void setPosition(Point2D position) {
+    this.position = position;
+  }
+
+  public double getWidth() {
+    return width;
+  }
+
+  public double getHeight() {
+    return height;
+  }
+
+  public boolean isActive() {
+    return isActive;
+  }
+
+  public void setActive(boolean active) {
+    isActive = active;
+  }
+}
